917562cf85f5e8fdc5ae12045e984521cafd829e
KENTO <kent08matsu1028@gmail.com>
2026-05-25 16:59:50 +0900

    phase6まで

---
+++ public/config.js
@@ -0,0 +1,7 @@
+window.__JAZZIN_SUPABASE_URL__  = 'https://kzdsrysnngnfupmszzvz.supabase.co';
+window.__JAZZIN_SUPABASE_ANON__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6ZHNyeXNubmduZnVwbXN6enZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MDMyMjYsImV4cCI6MjA5NTA3OTIyNn0.9smzTzWG-ImlrslqpL-gZe9OF5rnAK4E15oeny2U3Xo';
+
+// EmailJS（送信したい場合のみ設定）
+window.__JAZZIN_EMAILJS_PUBLIC_KEY__  = 'ijWoJrjae-kj4F5r-';
+window.__JAZZIN_EMAILJS_SERVICE_ID__  = 'service_qumzx3r';
+window.__JAZZIN_EMAILJS_TEMPLATE_ID__ = 'template_bexwqcp';
